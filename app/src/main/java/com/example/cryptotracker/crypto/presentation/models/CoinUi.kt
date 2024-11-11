95c4b20074b42224e39d7ff2fa987cc209629027
gfsilva <gfsilva@guarany.com.br>
2024-11-11 18:01:04 -0300

    first commit

---
+++ app/src/main/java/com/example/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -0,0 +1,45 @@
+package com.example.cryptotracker.crypto.presentation.models
+
+import androidx.annotation.DrawableRes
+import com.example.cryptotracker.crypto.domain.Coin
+import com.example.cryptotracker.core.presentation.util.getDrawableIdForCoin
+import java.text.NumberFormat
+import java.util.Locale
+
+data class CoinUi(
+    val id: String,
+    val rank: Int,
+    val name: String,
+    val symbol: String,
+    val marketCapUsd: DisplayableNumber,
+    val priceUsd: DisplayableNumber,
+    val changePercent24Hr: DisplayableNumber,
+    @DrawableRes val iconRes: Int
+)
+
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        name = name,
+        symbol = symbol,
+        rank = rank,
+        priceUsd = priceUsd.toDisplayableNumber(),
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumIntegerDigits = 2
+    }
+
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
---
+++ app/src/main/java/com/example/cryptotracker/crypto/presentation/models/DisplayableNumber.kt
@@ -0,0 +1,6 @@
+package com.example.cryptotracker.crypto.presentation.models
+
+data class DisplayableNumber(
+    val value: Double,
+    val formatted: String
+)
